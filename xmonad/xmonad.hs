033c744379ee6015c534b8218a9b82a82cefe3f9
scoiatael <czapl.luk@gmail.com>
2013-09-04 17:04:49 +0100

    reworked dzen bar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,12 +1,3 @@
---
--- xmonad example config file.
---
--- A template showing all available configuration hooks,
--- and how to override the defaults in your own xmonad.hs conf file.
---
--- Normally, you'd only override those defaults you care about.
---
-
 import XMonad
 import Data.Monoid
 import System.Exit
@@ -24,129 +15,87 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Layout.PerWorkspace (onWorkspace)
 import XMonad.Layout.Spiral
 import XMonad.Layout.SimpleFloat
+import XMonad.Layout.LayoutModifier
+--import XMonad.Config.Xfce
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.Monitor
 import Data.Ratio
 
--- The preferred terminal program, which is used in a binding below and by
--- certain contrib modules.
---
 myTerminal      = "lxterminal"
 
--- Whether focus follows the mouse pointer.
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse =True
 
--- Whether clicking on a window to focus also passes the click to the window
 myClickJustFocuses :: Bool
 myClickJustFocuses = False
 
--- Width of the window border in pixels.
---
 myBorderWidth   = 1
 
--- modMask lets you specify which modkey you want to use. The default
--- is mod1Mask ("left alt").  You may also consider using mod3Mask
--- ("right alt"), which does not conflict with emacs keybindings. The
--- "windows key" is usually mod4Mask.
---
 myModMask       = mod4Mask
 
--- The default number of workspaces (virtual screens) and their names.
--- By default we use numeric strings, but any string may be used as a
--- workspace name. The number of workspaces is determined by the length
--- of this list.
---
--- A tagging example:
---
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
---
-myWorkspaces    = ["1:main","2:web","3:floats","4","5","6","7","8","9"]
+myWorkspaces    = clickable. (map dzenEscape) $ ["1","2","3","4","5"]    
+  where clickable l     = [ "^ca(1,xdotool key super+" ++ show (n) ++ ")" ++ ws ++ "^ca()" |
+                            (i,ws) <- zip [1..] l,
+                            let n = i ]
+ 
 
--- Border colors for unfocused and focused windows, respectively.
---
 myNormalBorderColor  = "#dddddd"
 myFocusedBorderColor = "#ff0000"
 
-------------------------------------------------------------------------
--- Key bindings. Add, modify or remove key bindings here.
---
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
-    -- launch dmenu
     , ((modm,               xK_p     ), spawn "dmenu_run")
 
-    -- launch gmrun
     , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
 
-    -- close focused window
     , ((modm .|. shiftMask, xK_c     ), kill)
 
-     -- Rotate through the available layout algorithms
     , ((modm,               xK_space ), sendMessage NextLayout)
 
-    --  Reset the layouts on the current workspace to default
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
 
-    -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
 
-    -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
 
-    -- Move focus to the next window
     , ((modm,               xK_j     ), windows W.focusDown)
 
-    -- Move focus to the previous window
     , ((modm,               xK_k     ), windows W.focusUp  )
 
-    -- Move focus to the master window
     , ((modm,               xK_m     ), windows W.focusMaster  )
 
-    -- Swap the focused window and the master window
     , ((modm,               xK_Return), windows W.swapMaster)
 
-    -- Swap the focused window with the next window
     , ((modm .|. shiftMask, xK_j     ), windows W.swapDown  )
 
-    -- Swap the focused window with the previous window
     , ((modm .|. shiftMask, xK_k     ), windows W.swapUp    )
 
-    -- Shrink the master area
     , ((modm,               xK_h     ), sendMessage Shrink)
 
-    -- Expand the master area
     , ((modm,               xK_l     ), sendMessage Expand)
 
-    -- Push window back into tiling
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
 
-    -- Increment the number of windows in the master area
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
 
-    -- Deincrement the number of windows in the master area
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
 
-    -- Toggle the status bar gap
-    -- Use this binding with avoidStruts from Hooks.ManageDocks.
-    -- See also the statusBar function from Hooks.DynamicLog.
-    --
+    
     , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
-    -- Quit xmonad
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
-    -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
     , ((modm .|. shiftMask, xK_l     ), spawn "xscreensaver-command -lock | logger")
     ]
     ++
 
-    --
-    -- mod-[1..9], Switch to workspace N
-    -- mod-shift-[1..9], Move client to workspace N
-    --
+    
+
+    
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
@@ -156,149 +105,89 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. controlMask,   xK_Left      ), prevWS)
     , ((modm .|. shiftMask,     xK_Left      ), shiftToPrev)]
     ++
-    --
-    -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-    -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-    --
+    
+
+    
     [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
-
-------------------------------------------------------------------------
--- Mouse bindings: default actions bound to mouse events
---
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 
-    -- mod-button1, Set the window to floating mode and move by dragging
     [ ((modm, button1), (\w -> focus w >> mouseMoveWindow w
                                        >> windows W.shiftMaster))
 
-    -- mod-button2, Raise the window to the top of the stack
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
 
-    -- mod-button3, Set the window to floating mode and resize by dragging
     , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
                                        >> windows W.shiftMaster))
 
-    -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
 
-------------------------------------------------------------------------
--- Layouts:
+myLayout = ModifiedLayout hidePanel $ avoidStruts $ smartBorders $ onWorkspace "3" custom3 $ onWorkspace "2" custom2 $ custom1
 
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
-myLayout = onWorkspace "3:floats" custom3 $ onWorkspace "2:web" custom2 $ custom1
+hidePanel = monitor {  prop = ClassName "xfce4-panel", rect = Rectangle 0 0 0 0 }
 
-     -- default tiling algorithm partitions the screen into two panes
 myTiled   = Tall nmaster delta ratio
   where
-     -- The default number of windows in the master pane
+
      nmaster = 1
-     -- Default proportion of screen occupied by master pane
+
      ratio   = 1/2
-     -- Percent of screen to increment by when resizing panes
+
      delta   = 3/100
 
-custom1 = avoidStruts $ myTiled ||| Mirror myTiled ||| Full ||| spiral (1 % 1)
-custom2 = avoidStruts $ Mirror myTiled ||| myTiled ||| Full
-custom3 = avoidStruts $ simpleFloat ||| Full ||| Mirror myTiled ||| myTiled
-------------------------------------------------------------------------
--- Window rules:
+custom1 = myTiled ||| Mirror myTiled ||| Full ||| spiral (1 % 1)
+custom2 = Mirror myTiled ||| myTiled ||| Full
+custom3 = simpleFloat ||| Full ||| Mirror myTiled ||| myTiled
 
--- Execute arbitrary actions and WindowSet manipulations when managing
--- a new window. You can use this to, for example, always float a
--- particular program, or have a client always appear on a particular
--- workspace.
---
--- To find the property name associated with a program, use
--- > xprop | grep WM_CLASS
--- and click on the client you're interested in.
---
--- To match on the WM_NAME, you can use 'title' in the same way that
--- 'className' and 'resource' are used below.
---
-myManageHook = manageDocks <+> composeAll ( concat (
-    [ [className =? c       --> doShift "2:web" | c <- myWebs]
+myManageHook = manageMonitor hidePanel <+> manageDocks <+> composeAll ( concat (
+    [ [className =? c       --> doShift "2" | c <- myWebs]
     , [className =? "Conky"          --> doIgnore]
-    , [ className =? f        --> doShift "3:floats"  | f <- myFloats]
+    , [className =? "xfce4-panel" --> doIgnore]
+    , [resource =? "Dialog" --> doFloat]
+    , [ className =? f        --> doShift "3"  | f <- myFloats]
     ] ))
 
 myWebs = ["Chromium"]
 myFloats = ["Angband", "DeaDBeeF"]
-------------------------------------------------------------------------
--- Event handling
 
--- * EwmhDesktops users should change this to ewmhDesktopsEventHook
---
--- Defines a custom handler function for X Events. The function should
--- return (All True) if the default handler is to be run afterwards. To
--- combine event hooks use mappend or mconcat from Data.Monoid.
---
-myEventHook = docksEventHook
+myEventHook = ewmhDesktopsEventHook
 
-------------------------------------------------------------------------
--- Status bars and logging
-
--- Perform an arbitrary action on each internal state change or X event.
--- See the 'XMonad.Hooks.DynamicLog' extension for examples.
-{--
-myLogHook h = dynamicLogWithPP $ defaultPP
-    {
-        ppCurrent           =   dzenColor "#ebac54" "#1B1D1E" . pad
-      , ppVisible           =   dzenColor "white" "#1B1D1E" . pad
-      , ppHidden            =   dzenColor "white" "#1B1D1E" . pad
-      , ppHiddenNoWindows   =   dzenColor "#7b7b7b" "#1B1D1E" . pad
-      , ppUrgent            =   dzenColor "#ff0000" "#1B1D1E" . pad
-      , ppWsSep             =   " "
-      , ppSep               =   "  |  "
-      , ppLayout            =   dzenColor "#ebac54" "#1B1D1E" .
-                                (\x -> case x of
-                                    "ResizableTall"             ->      "^i(" ++ myBitmapsDir ++ "/tall.xbm)"
-                                    "Mirror ResizableTall"      ->      "^i(" ++ myBitmapsDir ++ "/mtall.xbm)"
-                                    "Full"                      ->      "^i(" ++ myBitmapsDir ++ "/full.xbm)"
-                                    "Simple Float"              ->      "~"
-                                    _                           ->      x
-                                )
-      , ppTitle             =   (" " ++) . dzenColor "white" "#1B1D1E" . dzenEscape
-      , ppOutput            =   hPutStrLn h
+myDzenPP  = dzenPP
+    { ppCurrent = dzenColor "#3399ff" "" . wrap " " " "
+    , ppHidden  = dzenColor "#dddddd" "" . wrap " " " "
+    , ppHiddenNoWindows = dzenColor "#777777" "" . wrap " " " "
+    , ppUrgent  = dzenColor "#ff0000" "" . wrap " " " "
+    , ppSep     = " | "
+    , ppLayout  = dzenColor "#aaaaaa" "" . wrap "^ca(1,xdotool key super+space)· " " ·^ca()"
+    , ppTitle   = dzenColor "#ffffff" "" 
+                    . wrap "^ca(1,xdotool key super+k)^ca(2,xdotool key super+shift+c)"
+                           "                          ^ca()^ca()" . shorten 15 . dzenEscape
     }
---}
-------------------------------------------------------------------------
--- Startup hook
+ 
+myLogHook myDzen = dynamicLogWithPP $ myDzenPP { 
+  ppOutput = hPutStrLn myDzen,
+  ppExtras = [wrapL "bat: " "" battery, 
+              aumixVolume, 
+              date "%b %d %H:%M"] }
 
--- Perform an arbitrary action each time xmonad starts or is restarted
--- with mod-q.  Used by, e.g., XMonad.Layout.PerWorkspace to initialize
--- per-workspace layout choices.
---
--- By default, do nothing.
-myStartupHook = do
-  spawn "xscreensaver &"
+myStartupHook = ewmhDesktopsStartup >> do
   spawn $ "conky -qdc " ++ myConfigDir ++ "conkyrc1"
   spawn $ "conky -qdc " ++ myConfigDir ++ "conkyrc2"
   spawn "sleep 1 && lxterminal"
-------------------------------------------------------------------------
--- Now run xmonad with all the defaults we set up.
 
--- Run xmonad with the settings you specify. No need to modify this.
---
-myXmonadBar = "dzen2 -x '1440' -y '0' -h '24'  -w '1280' -ta 'l' -fg '#FFFFFF' -bg '#1B1D1E'"
-myStatusBar = "i3status -c " ++ myConfigDir ++ "/i3status.conf | dzen2 -x '2080' -w '1280'  -h '24' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '24'"
+myXmonadBar = "dzen2 -y '0' -h '24'  -w '512' -ta 'l'" ++ myDzenStyle
+myStatusBar = "i3status -c " ++ myConfigDir ++ "/i3status.conf | dzen2 -w '512' -x '512'  -h '24' -ta 'r' -y '0'" ++ myDzenStyle
 myConfigDir = "$HOME/.xmonad/"
 myBitmapsDir = "$HOME/.xmonad/dzen2"
+myDzenStyle  = " -h '20' -fg '#777777' -bg '#222222' -fn 'arial:bold:size=11'"
 
 main = do
   myDzen <- spawnPipe myXmonadBar
   dzenRightBar <- spawnPipe myStatusBar
   xmonad defaultConfig {
-      -- simple stuff
+
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
         clickJustFocuses   = myClickJustFocuses,
@@ -308,16 +197,12 @@ main = do
         normalBorderColor  = myNormalBorderColor,
         focusedBorderColor = myFocusedBorderColor,
 
-      -- key bindings
         keys               = myKeys,
         mouseBindings      = myMouseBindings,
 
-      -- hooks, layouts
         layoutHook         = myLayout,
         manageHook         = myManageHook,
         handleEventHook    = myEventHook,
-        --logHook            = myLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd,
-        logHook            = dynamicLogWithPP $ byorgeyPP { ppOutput = hPutStrLn myDzen,
-          ppExtras = [wrapL "bat: " "" battery, aumixVolume, date "%c"] },
+        logHook            = myLogHook myDzen >> fadeInactiveLogHook 0xdddddddd,
         startupHook        = myStartupHook
     }
