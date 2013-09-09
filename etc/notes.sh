d2a9ec0d70cb3fabd7175a2ef9de8c454aa7d276
scoiatael <czapl.luk@gmail.com>
2013-09-09 23:58:00 +0200

    Changed colours, made notes shell script

--- etc/notes.sh
+++ etc/notes.sh
@@ -1,11 +1,21 @@
-#lightdm
+#!/bin/sh
+## MANUAL:
+# add wallpaper to /usr/share/backgrounds/default.jpg
+# add 'nosplash' to boot options
+##
+
+#lightdm ->
 chown -hR lightdm:lightdm /var/lib/lightdm
-#add wallpaper to /usr/share/backgrounds/default.jpg
+tar czf lightdm.tgz.bp /etc/lightdm/* 
+tar xzf lightdm.tgz -C /
 #rsyslog -> 
 chown 0644 /var/log/messages
+tar czf rsyslog.tgz.bp /etc/rsyslog.conf
+tar xzf rsyslog.tgz -C /
 #net names -> 
 ln -s /dev/null /etc/polkit-1/rules.d/80-net-name-slot.rules
 #volume -> 
 modprobe snd_mixer_oss
 #copy/paste -> 
 cp urxvtclip /usr/lib/urxvt/perl/clipboard
+
