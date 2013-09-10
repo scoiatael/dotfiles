7c6c800c8546ba717230e514d58bfc11761a3107
scoiatael <czapl.luk@gmail.com>
2013-09-10 17:35:06 +0100

    fixed update_links

--- update_links.sh
+++ update_links.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-LINKDIR="$DIR/dotfiles/"
+LINKDIR="$DIR/dotfiles"
 
 for LINKFILE in $( ls $LINKDIR/*.link ) 
 do
-	LINKPATH=$DIR/$( cat $LINKDIR/$LINKFILE )
+	LINKPATH=$DIR/$( cat $LINKFILE )
 	LINKNAME=${LINKFILE%.link}
-	rm -fr $HOME/.$LINKNAME
-	ln -fvs $LINKPATH $HOME/.$LINKNAME
+  rm -fr $HOME/.$( basename $LINKNAME )
+  ln -fvs $LINKPATH $HOME/.$( basename $LINKNAME )
 done
