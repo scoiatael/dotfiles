94c5fb35a97d244b5669012fd15ee6a79ac72f77
scoiatael <czapl.luk@gmail.com>
2013-09-22 17:33:47 +0200

    more fixes

--- etc/notes.sh
+++ etc/notes.sh
@@ -6,13 +6,13 @@
 
 #lightdm ->
 chown -hR lightdm:lightdm /var/lib/lightdm
-tar czf lightdm.tgz.bp /etc/lightdm/* 
-tar xzf lightdm.tgz -C /
+tar czf etc/lightdm.tgz.bp /etc/lightdm/* 
+tar xzf etc/lightdm.tgz -C /
 systemctl enable lightdm
 #rsyslog -> 
 chown 0644 /var/log/messages
-tar czf rsyslog.tgz.bp /etc/rsyslog.conf
-tar xzf rsyslog.tgz -C /
+tar czf etc/rsyslog.tgz.bp /etc/rsyslog.conf
+tar xzf etc/rsyslog.tgz -C /
 systemctl enable rsyslog
 #net names -> 
 ln -s /dev/null /etc/polkit-1/rules.d/80-net-name-slot.rules
