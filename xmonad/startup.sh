c41fcdb23c22bb24a73bbc7de73cbac80e2d4e9a
scoiatael <czapl.luk@gmail.com>
2014-03-09 00:55:35 +0100

    redone conky

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -2,11 +2,12 @@
 
 myConfigDir=$HOME/.xmonad/confs/
 myTerminal=/usr/bin/urxvtc
+# Arch Linux lua-cairo fix
+luaDir=/usr/lib/lua/5.1/
 
 xscreensaver -no-splash &
 sleep 1 && urxvtd &
-sleep 1 && conky -c "$myConfigDir"conkyMessages.conf &
-sleep 1 && conky -c "$myConfigDir"conkyStats.conf &
+sleep 1 && cd $luaDIR && conky -c "$myConfigDir"conkyStats.conf &
 sleep 1 && $myTerminal &
 syndaemon -k -d -t 1.0 &
 setxkbmap pl
