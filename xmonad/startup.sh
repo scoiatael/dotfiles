73101df460aa3cdb5bb6b71fcfe711cc1f148cf7
scoiatael <czapl.luk@gmail.com>
2015-04-01 21:31:18 +0000

    update

--- xmonad/startup.sh
+++ xmonad/startup.sh
@@ -5,7 +5,7 @@ myTerminal=/usr/bin/urxvtc
 
 ( echo "Merging $HOME/.Xdefaults" && xrdb $HOME/.Xdefaults ) | logger &
 sleep 1 && (pgrep urxvtd || urxvtd) &
-sleep 1 && conky -c "$myConfigDir"conkyStats.conf &
+#sleep 1 && conky -c "$myConfigDir"conkyStats.conf &
 sleep 2 && (pgrep urxvtc || urxvtc) &
 syndaemon -k -d -t 1.0 &
 setxkbmap pl -option "caps:swapescape" &
