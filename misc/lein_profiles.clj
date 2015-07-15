bf9d6f56ca8365749a1585f9b1fb5e37c68671ec
scoiatael <eternal.kadir@gmail.com>
2015-07-15 11:00:03 +0200

    added lein profiles

---
+++ misc/lein_profiles.clj
@@ -0,0 +1,6 @@
+{:user  {:plugins  [[lein-vanity  "0.2.0"]
+                    [lein-try  "0.4.3"]
+                    [lein-checkall  "0.1.1"]
+                    [lein-ancient  "0.6.7"]
+                    [mvxcvi/whidbey  "1.0.0"]
+                    [lein-plz  "0.4.0-SNAPSHOT" :exclusions  [[rewrite-clj]  [ancient-clj]]]]}}
