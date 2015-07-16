409a9b0112b7b26bbce9e5ebf7e75407eff87f82
scoiatael <eternal.kadir@gmail.com>
2015-07-16 11:05:18 +0200

    added cloc to lein profiles, for documentation

--- misc/lein_profiles.clj
+++ misc/lein_profiles.clj
@@ -2,5 +2,6 @@
                     [lein-try  "0.4.3"]
                     [lein-checkall  "0.1.1"]
                     [lein-ancient  "0.6.7"]
+                    [cloc  "0.1.0"]
                     [mvxcvi/whidbey  "1.0.0"]
                     [lein-plz  "0.4.0-SNAPSHOT" :exclusions  [[rewrite-clj]  [ancient-clj]]]]}}
