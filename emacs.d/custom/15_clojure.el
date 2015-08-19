5eaeef3c5651adaf6fee26d97ef6e9d908eb1345
L <lukasz.czaplinski@tooploox.com>
2015-08-19 14:10:41 +0200

    fixd clash between evil-lispy and evil-surround

--- emacs.d/custom/15_clojure.el
+++ emacs.d/custom/15_clojure.el
@@ -1,15 +1,18 @@
 (require 'clj-refactor)
 (require 'clojure-mode)
+(setq evil-lispy-modified-operators nil)
+(require 'evil-lispy)
 
 (defun scoiatael/clj-mode-hook ()
   (clj-refactor-mode 1)
   (yas-minor-mode 1)
   (evil-lispy-mode 1)
-  ;(lispy-mode 1)
+					;(lispy-mode 1)
   (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(defun scoiatael/emacs-lisp-mode-hook ()
+  (evil-lispy-mode t))
 
 (add-hook 'clojure-mode-hook #'scoiatael/clj-mode-hook)
 
-(add-hook 'emacs-lisp-mode-hook (lambda () (evil-lispy-mode 1)))
-
-(require 'evil-lispy)
+(add-hook 'emacs-lisp-mode-hook #'scoiatael/emacs-lisp-mode-hook)
