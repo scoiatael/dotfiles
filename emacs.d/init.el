2c5480f18730bb588bb3e7047a270196308f1ae8
scoiatael <czapl.luk@gmail.com>
2015-08-24 08:57:30 +0000

    emacs backup settings(?)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -30,5 +30,14 @@
 (show-paren-mode 1)
 (electric-pair-mode 1)
 
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacs.d/saves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+
 (provide 'init)
 ;;; init.el ends here
