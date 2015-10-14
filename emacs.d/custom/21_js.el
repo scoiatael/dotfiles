cd927c7526651d3b5f5fd6ccbaaeda8776c974aa
L <lukasz.czaplinski@tooploox.com>
2015-10-14 21:22:59 +0200

    javascript, llvm-mode

---
+++ emacs.d/custom/21_js.el
@@ -0,0 +1,15 @@
+;; use web-mode for .jsx files
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(json-jsonlist)))
