58e1adfed3a5aadbb7efc9176f39ca2a4521c2ab
scoiatael <czapl.luk@gmail.com>
2016-02-27 17:27:36 +0100

    xmonad float krunner

--- haskell/xmonad.hs
+++ haskell/xmonad.hs
@@ -1,19 +1,19 @@
 import XMonad
 import XMonad.Config.Kde
 import qualified XMonad.StackSet as W -- to shift and float windows
- 
+
 main = xmonad kde4Config
     { modMask = mod4Mask -- use the Windows button as mod
     , manageHook = manageHook kdeConfig <+> myManageHook
     }
- 
+
 myManageHook = composeAll . concat $
     [ [ className   =? c --> doFloat           | c <- myFloats]
     , [ title       =? t --> doFloat           | t <- myOtherFloats]
     , [ className   =? c --> doF (W.shift "2") | c <- webApps]
     , [ className   =? c --> doF (W.shift "3") | c <- ircApps]
     ]
-  where myFloats      = ["MPlayer", "Gimp"]
+  where myFloats      = ["MPlayer", "Gimp", "krunner"]
         myOtherFloats = ["alsamixer"]
         webApps       = ["Firefox-bin", "Opera"] -- open on desktop 2
         ircApps       = ["Ksirc"]                -- open on desktop 3
