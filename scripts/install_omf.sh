fdd87a4409981a88249533e031d3cc76d8ecd23c
scoiatael <czapl.luk@gmail.com>
2016-05-29 19:33:23 +0200

    fix omf install for fish

--- scripts/install_omf.sh
+++ scripts/install_omf.sh
@@ -1,5 +1,6 @@
-TMP=$(mktmp)
-SCRIPT_PATH="$TMP/install"
+#!/bin/sh
+TMP=$(mktemp)
+SCRIPT_PATH="$TMP"
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > $SCRIPT_PATH
 less $SCRIPT_PATH
 echo "Run 'fish $SCRIPT_PATH' to install omf"
