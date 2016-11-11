fcbd549addf402ca678dfb40ad5ba3af3f5d5408
scoiatael <czapl.luk@gmail.com>
2016-11-11 18:39:20 +0100

    Use own prompt, copy from omf/theme-cmorrel.com

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 set -U fish_key_bindings fish_vi_key_bindings
+function fish_mode_prompt; end
 
 for file in ~/.config/fish/conf.d/*.fish
     source $file
@@ -11,10 +12,6 @@ eval (python -m virtualfish auto_activation compat_aliases)
 set -l iterm_integration $HOME/.iterm2_shell_integration.fish
 if test -f $iterm_integration
   source $iterm_integration
-end
-
-function gitpr
-  git branch --merged master | grep -v 'master$' | xargs git branch -d
 end
 
 set PATH $PATH /usr/local/opt/go/libexec/bin
