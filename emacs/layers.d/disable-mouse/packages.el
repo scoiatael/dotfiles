75619862c7aafaa10a0dc9efe3a49c1fad315073
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2018-11-19 22:34:25 +0100

    Refresh .spacemacs; disable-mouse

---
+++ emacs/layers.d/disable-mouse/packages.el
@@ -0,0 +1,65 @@
+;;; packages.el --- disable-mouse layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2018 Sylvain Benner & Contributors
+;;
+;; Author: Łukasz Czapliński <lukaszczaplinski@LsMBP.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Commentary:
+
+;; See the Spacemacs documentation and FAQs for instructions on how to implement
+;; a new layer:
+;;
+;;   SPC h SPC layers RET
+;;
+;;
+;; Briefly, each package to be installed or configured by this layer should be
+;; added to `disable-mouse-packages'. Then, for each package PACKAGE:
+;;
+;; - If PACKAGE is not referenced by any other Spacemacs layer, define a
+;;   function `disable-mouse/init-PACKAGE' to load and initialize the package.
+
+;; - Otherwise, PACKAGE is already referenced by another Spacemacs layer, so
+;;   define the functions `disable-mouse/pre-init-PACKAGE' and/or
+;;   `disable-mouse/post-init-PACKAGE' to customize the package as it is loaded.
+
+;;; Code:
+
+(defconst disable-mouse-packages
+  '(disable-mouse)
+  "The list of Lisp packages required by the disable-mouse layer.
+
+Each entry is either:
+
+1. A symbol, which is interpreted as a package to be installed, or
+
+2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
+    name of the package to be installed or loaded, and KEYS are
+    any number of keyword-value-pairs.
+
+    The following keys are accepted:
+
+    - :excluded (t or nil): Prevent the package from being loaded
+      if value is non-nil
+
+    - :location: Specify a custom installation location.
+      The following values are legal:
+
+      - The symbol `elpa' (default) means PACKAGE will be
+        installed using the Emacs package manager.
+
+      - The symbol `local' directs Spacemacs to load the file at
+        `./local/PACKAGE/PACKAGE.el'
+
+      - A list beginning with the symbol `recipe' is a melpa
+        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+
+(defun disable-mouse/init-disable-mouse ()
+  (global-disable-mouse-mode)
+  )
+
+;;; packages.el ends here
