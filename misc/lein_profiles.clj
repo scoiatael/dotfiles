27850f733f12e6bab48e921d61a3b39fa052f00f
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2019-05-01 15:59:18 +0200

    Cleanup leiningen tools

--- misc/lein_profiles.clj
+++ misc/lein_profiles.clj
@@ -1,11 +1,2 @@
-{:user  {:plugins  [[lein-vanity  "0.2.0"]
-                    [lein-try  "0.4.3"]
-                    [lein-ancient  "0.6.7"]
-                    [lein-bikeshed "0.2.0"]
-                    [lein-kibit "0.1.2"]
-                    [jonase/eastwood "0.2.1"]
-                    [cloc  "0.1.0"]
-                    [mvxcvi/whidbey  "1.0.0"]
-                    [acyclic/squiggly-clojure "0.1.3-SNAPSHOT"]
-                    [lein-plz  "0.4.0-SNAPSHOT" :exclusions  [[rewrite-clj]  [ancient-clj]]]]}
- :repl {:plugins [[cider/cider-nrepl "0.10.0"]]}}
+{:user  {:plugins  []}
+ :repl {:plugins []}}
