3d5d6db7eddaa0efa9123420eccb0180c439458a
lukasz.czaplinski <lczaplinski@opera.com>
2020-02-18 10:22:33 +0100

    Ditch customization via links for gitconfig

--- scripts/update_links.rb
+++ scripts/update_links.rb
@@ -25,7 +25,6 @@ Linker.create! do
 
   group 'git' do
     link 'gitconfig'
-    link 'gitconfig_custom', to: "#{current_group}/gitconfig_custom_#{os}"
     link 'gitignore_global', to: "#{current_group}/gitignore"
   end
 
