3023978b5a5975a6007e67561d491c42e38d4a57
Łukasz Czapliński <lczaplinski@opera.com>
2020-10-15 15:37:49 +0200

    Improve snippets for Puppet

--- emacs/doom.d/autoload/puppet.el
+++ emacs/doom.d/autoload/puppet.el
@@ -4,7 +4,10 @@
 ;;;###autoload
 (defun scoiatael/puppet-default-namespace ()
   "Use relative file name to infer namespace for element"
-  (s-replace "/" "::" (file-name-directory (scoiatael/file-relative-name))))
+  (let* ((dir (file-name-directory (scoiatael/file-relative-name)))
+        (dir-no-modules (s-replace "modules/" "" dir))
+        (dir-no-manifests (s-replace "manifests/" "" dir-no-modules)))
+    (s-replace "/" "::" dir-no-manifests)))
 
 ;;;###autoload
 (defun scoiatael/puppet-align-parameters ()
