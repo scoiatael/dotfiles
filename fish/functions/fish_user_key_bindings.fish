2e5ac8cf3620ebdf2e9cc9f3b9201521646f30db
Łukasz Czapliński <lczaplinski@opera.com>
2021-06-09 15:27:36 +0200

    Add keybind for fzf_z

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -2,4 +2,6 @@ function fish_user_key_bindings
     bind -M insert \cs forward-char
     bind -M insert \cp 'prevd > /dev/null; commandline -f repaint'
     bind -M insert \cn 'nextd > /dev/null; commandline -f repaint'
+
+    bind \cz __fzf_z
 end
