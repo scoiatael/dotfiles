b9eaf208a8e6d36c542896599fe670f804d0bd2d
Łukasz Czapliński <lczaplinski@opera.com>
2021-07-19 10:41:11 +0200

    Rename window for tmux

--- bin/__fzf_ssh.sh
+++ bin/__fzf_ssh.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 HOST=$(
-    awk '{ split($1, A, ","); print(A[1]) }' < "$HOME/.ssh/known_hosts" |
-    fzf-tmux-popup \
-         --preview 'host {}' \
-         --preview-window "bottom:1:wrap"
+    awk '{ split($1, A, ","); print(A[1]) }' <"$HOME/.ssh/known_hosts" |
+        fzf-tmux-popup \
+            --preview 'host {}' \
+            --preview-window "bottom:1:wrap"
 )
 
+tmux rename-window $HOST
 exec ssh "$HOST"
--- bin/__fzf_z.sh
+++ bin/__fzf_z.sh
@@ -2,12 +2,11 @@
 
 DIR=$(
     zoxide query |
-    fzf-tmux-popup \
-         --preview 'exa -ld --git {}' \
-         --preview-window "bottom:1:wrap"
+        fzf-tmux-popup \
+            --preview 'exa -ld --git {}' \
+            --preview-window "bottom:1:wrap"
 )
 
 cd "$DIR" || exit 1
+tmux rename-window $DIR
 exec $SHELL
-
-
