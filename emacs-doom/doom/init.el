80cada3507e3305a8bd6d0106d2420f7dabe299f
Łukasz Czapliński <lczaplinski@opera.com>
2021-12-02 13:31:25 +0100

    Start using forge

--- emacs-doom/doom/init.el
+++ emacs-doom/doom/init.el
@@ -100,7 +100,7 @@
         ;; +docsets
                                        ; ...or in Dash docsets locally
        (lsp +peek)
-       magit                    ; a git porcelain for Emacs
+       (magit +forge)                    ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
