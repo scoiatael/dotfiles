2031232144568a98431b983ce3c046ffb81ce5c1
Łukasz Czapliński <lczaplinski@opera.com>
2021-12-21 14:19:53 +0100

    Upgrade also hide/show

--- bin/__hide_show_alacritty.sh
+++ bin/__hide_show_alacritty.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env elvish
 
-set ALACRITTY_FOCUSED = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .focused')
-set ALACRITTY_ID = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .id')
+var ALACRITTY_FOCUSED = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .focused')
+var ALACRITTY_ID = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .id')
 if ?(test 1 -eq $ALACRITTY_FOCUSED) {
   yabai -m window --minimize $ALACRITTY_ID
   exec yabai -m window --focus last
