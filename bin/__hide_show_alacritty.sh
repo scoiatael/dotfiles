e112714f968e2df02cee284d983ad516516cfa69
Łukasz Czapliński <lczaplinski@opera.com>
2022-01-10 11:39:38 +0100

    ditch jq

--- bin/__hide_show_alacritty.sh
+++ bin/__hide_show_alacritty.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env elvish
 
-var ALACRITTY_FOCUSED = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .focused // ."has-focus"')
-var ALACRITTY_ID = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .id')
-if (or (==s true $ALACRITTY_FOCUSED) (== 1 $ALACRITTY_FOCUSED)) {
+use github.com/zzamboni/elvish-modules/util
+
+var ALACRITTY = (
+  yabai -m query --windows |
+    from-json |
+    util:select { |x| ==s $x['app'] Alacritty } (all)
+)
+var ALACRITTY_ID = (exact-num $ALACRITTY["id"])
+if (or (==s true (util:path-in $ALACRITTY ["has-focus"] &default=false)) (== 1 (exact-num (util:path-in $ALACRITTY ["focused"] &default=0)))) {
   yabai -m window --minimize $ALACRITTY_ID
   exec yabai -m window --focus last
 } else {
