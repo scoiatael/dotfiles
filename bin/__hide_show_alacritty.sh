b0020025bd695e8f9a6ebc6ae531d6c48da40763
Łukasz Czapliński <lczaplinski@opera.com>
2022-01-10 11:17:20 +0100

    make it work with non-HEAD yabai

--- bin/__hide_show_alacritty.sh
+++ bin/__hide_show_alacritty.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env elvish
 
-var ALACRITTY_FOCUSED = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | ."has-focus"')
+var ALACRITTY_FOCUSED = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .focused // ."has-focus"')
 var ALACRITTY_ID = (yabai -m query --windows | jq '.[] | select(.app=="Alacritty") | .id')
-if (==s true $ALACRITTY_FOCUSED) {
+if (or (==s true $ALACRITTY_FOCUSED) (== 1 $ALACRITTY_FOCUSED)) {
   yabai -m window --minimize $ALACRITTY_ID
   exec yabai -m window --focus last
 } else {
