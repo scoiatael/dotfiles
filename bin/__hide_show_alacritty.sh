8c50a268760b97f683492b6b0ff29b9264f7bff5
Lukasz Czaplinski <git@scoiatael.dev>
2022-01-21 17:57:36 +0100

    fixup! make it work with non-HEAD yabai

--- bin/__hide_show_alacritty.sh
+++ bin/__hide_show_alacritty.sh
@@ -8,7 +8,7 @@ var ALACRITTY = (
     util:select { |x| ==s $x['app'] Alacritty } (all)
 )
 var ALACRITTY_ID = (exact-num $ALACRITTY["id"])
-if (or (==s true (util:path-in $ALACRITTY ["has-focus"] &default=false)) (== 1 (exact-num (util:path-in $ALACRITTY ["focused"] &default=0)))) {
+if (or (eq $true (util:path-in $ALACRITTY ["has-focus"] &default=false)) (== 1 (exact-num (util:path-in $ALACRITTY ["focused"] &default=0)))) {
   yabai -m window --minimize $ALACRITTY_ID
   exec yabai -m window --focus last
 } else {
