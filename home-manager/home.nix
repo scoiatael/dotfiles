7ef470644b2114d0b05a0cc418253257b990645a
Lukasz Czaplinski <git@scoiatael.dev>
2022-04-24 14:42:39 +0200

    fix prefix highlight

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -317,7 +317,14 @@
         tmuxPlugins.extrakto
         tmuxPlugins.tmux-fzf
         tmuxPlugins.open
-        tmuxPlugins.prefix-highlight
+        {
+          plugin = tmuxPlugins.prefix-highlight;
+          extraConfig = ''
+            set-option -g status-right '#{prefix_highlight} #(echo "#{pane_title}" | cut -d" " -f1) #[fg=red,dim,bg=default]#(ps -o command -g "#{pane_pid}" | grep "ssh " | cut -d" " -f2-)#[fg=default,nodim,bg=default]#{?window_bigger,[#{window_offset_x}#,#{window_offset_y}] ,}'
+            set-option -g status-right-length 80
+            set-option -g status-position bottom
+          '';
+        }
       ];
       tmuxinator.enable = true;
       keyMode = "vi";
