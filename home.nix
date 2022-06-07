0d35157dbc6a437f2c1144c222e6670b7939bd0d
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-06-07 14:49:59 +0200

    tweak qutebrowser

--- home.nix
+++ home.nix
@@ -245,8 +245,19 @@
           nw = "https://nixos.wiki/index.php?search={}";
           g = "https://www.google.com/search?hl=en&q={}";
           nix = "https://search.nixos.org/packages?query={}";
-          kagi = "https://kagi.com/search?q={}";
+          k = "https://kagi.com/search?q={}";
+          DEFAULT = "https://kagi.com/search?q={}";
         };
+      settings = {
+        url.start_pages = "https://kagi.com";
+        url.default_page = "https://kagi.com";
+        colors.hints.bg = "qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 rgb(255, 247, 133), stop:1 rgb(255, 197, 66))";
+        colors.webpage.darkmode.enabled = true;
+        content.cookies.accept = "no-3rdparty";
+        content.default_encoding = "utf-8";
+        hints.uppercase = true;
+        zoom.default = "135%";
+      };
     };
     starship = {
       enable = true;
