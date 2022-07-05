60070e62bbce56356dfbbe7bdb3340aeb9322656
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-07-05 12:36:06 +0200

    fix elv helpers

--- bin/monitor.sh
+++ bin/monitor.sh
@@ -14,13 +14,20 @@ if (not (has-external notify-send)) {
 var br = (xrandr --prop --verbose | grep -A10 " connected primary" | grep "Brightness" | awk '{ print $2 }')
 var dev = (xrandr --prop --verbose | grep " connected primary" | awk '{ print $1 }')
 
+fn round2 { |num|
+    use math
+
+    put (/ (math:round (* $num 10)) 10)
+}
+
 if (==s $args[0] up ) {
-    set br = (+ $br 0.1)
+    set br = (round2 (+ $br 0.1))
 }
 
 if (==s $args[0] down ) {
-    set br = (- $br 0.1)
+    set br = (round2 (- $br 0.1))
 }
 
+echo "Brightness: " $br
 xrandr --output $dev --brightness $br
 notify-send -i monitor -t 300 "Brightness: " $br
