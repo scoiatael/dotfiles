7c55a250682737e35cdeb8980e44ee4244444caf
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-08-23 12:36:55 +0200

    back to c...

--- config/doom/init.el
+++ config/doom/init.el
@@ -105,11 +105,11 @@
        tree-sitter       ; syntax and parsing, sitting in a tree...
        (lsp +peek)
        magit           ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       ;;rgb               ; creating color strings
+       rgb               ; creating color strings
        terraform  ; infrastructure as code
        tmux       ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
@@ -121,7 +121,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;assembly          ; assembly for fun or debugging
-       ;; (cc +lsp)                ; C/C++/Obj-C madness
+       (cc +lsp)                ; C/C++/Obj-C madness
        (clojure +lsp)   ; java with a lisp
        common-lisp      ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
