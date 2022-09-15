be2a4db7373d050c6eb6c6bd3b0ec7954d67908d
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-09-15 15:18:24 +0200

    s.el is awesome <3

--- config/doom/autoload/text.el
+++ config/doom/autoload/text.el
@@ -22,3 +22,23 @@
 (defun scoiatael/json-to-yaml (start end)
   (interactive (list (region-beginning) (region-end)))
   (shell-command-on-region start end "ruby -e 'require \"yaml\"; require \"json\"; puts YAML.dump(JSON.parse($stdin.read))'" :replace t))
+
+;;;###autoload
+(defun to-snake-case (start end)
+  "Change selected text to snake case format"
+  (interactive "r")
+  (if (use-region-p)
+      (let ((camel-case-str (buffer-substring start end)))
+        (delete-region start end)
+        (insert (s-snake-case camel-case-str)))
+    (message "No region selected")))
+
+;;;###autoload
+(defun to-camel-case (start end)
+  "Change selected text to snake case format"
+  (interactive "r")
+  (if (use-region-p)
+      (let ((str (buffer-substring start end)))
+        (delete-region start end)
+        (insert (s-upper-camel-case str)))
+    (message "No region selected")))
