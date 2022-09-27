a14af61cecf65c5fb6411599d49849923dca6816
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2022-09-27 11:38:42 +0200

    icons don't work well on sway apparently

--- config/doom/init.el
+++ config/doom/init.el
@@ -24,7 +24,7 @@
        ;; helm             ; the *other* search engine for love and life
        ;; ido               ; the other *other* search engine...
        ;; (ivy +childframe)                                 ; a search engine for love and life
-       (vertico +icons)
+       (vertico)
 
        :ui
        deft              ; notational velocity for Emacs
