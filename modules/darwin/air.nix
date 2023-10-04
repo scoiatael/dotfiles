91aa01b5f525e0f92a1c89ace0501f836ce2dec5
Lukasz Czaplinski <git@scoiatael.dev>
2023-10-04 11:22:56 +0200

    more macOS stuff

--- modules/darwin/air.nix
+++ modules/darwin/air.nix
@@ -4,10 +4,11 @@
   hardware.notch = true;
 
   homebrew = {
-    casks = [ "todoist" "arc" "skype" ];
+    casks = [ "todoist" "arc" "skype" "canon-eos-utility" "discord" ];
     masApps = {
       "bitwarden" = 1352778147;
       "kagi" = 1622835804;
+      "hush" = 1544743900;
     };
   };
 }
