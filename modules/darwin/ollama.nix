b602daf3df67c5db933ad8de9d0b3c0edfb7f64d
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2024-03-21 16:14:55 +0100

    add ollama to system packages for CLI options

--- modules/darwin/ollama.nix
+++ modules/darwin/ollama.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
+  environment.systemPackages = [ pkgs.ollama ];
   launchd.user.agents.ollama = {
     path = [ config.environment.systemPath ];
     serviceConfig.ProgramArguments = [ "${pkgs.ollama}/bin/ollama" "serve" ];
