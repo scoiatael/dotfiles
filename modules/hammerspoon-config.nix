3c0f6e60687da40c25ce44bdbfe74d36acd188e2
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2024-04-03 16:19:11 +0200

    One more doc

--- modules/hammerspoon-config.nix
+++ modules/hammerspoon-config.nix
@@ -6,6 +6,7 @@
     recursive = true;
   };
   # TODO: Port more stuff from https://github.com/rtauziac/Hammerspoon-Yabai/blob/master/.hammerspoon/init.lua
+  # TODO: Port more stuff from https://github.com/treffynnon/nix-setup/blob/master/home-configs/hammerspoon/init.lua
   home.file.".hammerspoon/init.lua".text = ''
     hs.loadSpoon("Yabai")
     hs.hotkey.bind({"alt"}, "t", function()
