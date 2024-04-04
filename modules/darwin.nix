a72135f223138db3485f4b329ccd9cb2afa8fcab
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2024-04-04 17:08:32 +0200

    Set some Darwin defaults in nix

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -38,6 +38,26 @@
   system.keyboard.remapCapsLockToControl =
     true; # Works alongside ControlEscape for better Caps :)
 
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.dock.orientation = "left";
+  system.defaults.dock.show-recents = false;
+  system.defaults.dock.static-only = true;
+
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.FXEnableExtensionChangeWarning = false;
+  system.defaults.finder.ShowStatusBar = true;
+
+  system.defaults.spaces.spans-displays = false;
+
+  system.defaults.".GlobalPreferences"."com.apple.mouse.scaling" = "-1.0";
+
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  system.defaults.NSGlobalDomain.NSDisableAutomaticTermination = true;
+  system.defaults.NSGlobalDomain."com.apple.sound.beep.volume" = 0.0;
+  system.defaults.NSGlobalDomain._HIHideMenuBar = true;
+
   # https://github.com/LnL7/nix-darwin/blob/master/modules/system/activation-scripts.nix#L111
   system.activationScripts.postUserActivation.text = let
     brewPrefix = if pkgs.stdenv.hostPlatform.isAarch64 then
@@ -48,6 +68,9 @@
     echo >&2 "setting up homebrew autoupdate..."
     PATH=${brewPrefix}:$PATH brew autoupdate delete > /dev/null
     PATH=${brewPrefix}:$PATH brew autoupdate start --upgrade --cleanup
+
+    # https://derflounder.wordpress.com/2023/09/26/managing-the-click-wallpaper-to-reveal-desktop-setting-in-macos-sonoma/
+    /usr/bin/defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
   '';
   # https://github.com/LnL7/nix-darwin/blob/master/modules/security/pam.nix#L25
   security.pam.enableSudoTouchIdAuth = true;
