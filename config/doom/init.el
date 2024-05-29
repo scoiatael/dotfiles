6cad825ce994ece7500263402d44b4b6429a1efd
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2024-05-29 13:06:09 +0200

    remove obsolete layers

--- config/doom/init.el
+++ config/doom/init.el
@@ -37,10 +37,10 @@
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
-       minimap           ; show a map of the code on the side
+       ;; minimap           ; show a map of the code on the side
        ;; modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash                  ; blink cursor line after big motions
-       neotree                    ; a project drawer, like NERDTree for vim
+       ;; neotree                    ; a project drawer, like NERDTree for vim
        ophints                    ; highlight the region an operation acts on
        (popup +defaults +all)          ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
