a30ce97935cc6038e85853230bfae54f873c9ad9
Lucas Czaplinski <lukasz.czaplinski@ceros.com>
2024-05-29 13:03:50 +0200

    nix flake update

--- config/doom/init.el
+++ config/doom/init.el
@@ -37,14 +37,14 @@
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
-       ;;minimap           ; show a map of the code on the side
+       minimap           ; show a map of the code on the side
        ;; modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash                  ; blink cursor line after big motions
        neotree                    ; a project drawer, like NERDTree for vim
        ophints                    ; highlight the region an operation acts on
-       (popup +defaults)          ; tame sudden yet inevitable temporary windows
+       (popup +defaults +all)          ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
-       (treemacs +lsp)               ; a project drawer, like neotree but cooler
+       treemacs               ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
        (vc-gutter +pretty)    ; vcs diff in the fringe
        vi-tilde-fringe        ; fringe tildes to mark beyond EOB
@@ -70,7 +70,7 @@
        :emacs
        (dired +ranger)   ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ibuffer           ; interactive buffer management
+       (ibuffer +icons)           ; interactive buffer management
        (undo +tree)      ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
