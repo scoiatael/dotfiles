23461e28877f9b704b248158a57dabca544da1db
Lukasz Czaplinski <git@scoiatael.dev>
2024-08-08 17:59:15 +0200

    improve gleam setup

--- config/doom/+private|gleam.el
+++ config/doom/+private|gleam.el
@@ -1,9 +1,30 @@
 ;;; ../../dotfiles/config/doom/gleam.el -*- lexical-binding: t; -*-
 
 (use-package gleam-ts-mode
-  :config (add-to-list 'auto-mode-alist '("\\.gleam\\'" . gleam-ts-mode)))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.gleam\\'" . gleam-ts-mode))
+  (add-hook 'gleam-ts-mode-hook
+            #'eglot-ensure)
+  (add-hook 'gleam-ts-mode-hook
+            (lambda () (add-hook 'before-save-hook 'eglot-format-buffer  nil t)))
+  (add-hook 'gleam-mode-hook
+            (lambda () (add-hook 'before-save-hook 'gleam-format nil t))))
 
 (after! apheleia (push '(gleam-format . ("gleam format" filepath)) apheleia-formatters))
+
+(after! projectile
+  (projectile-register-project-type 'gleam '("gleam.toml")
+                                    :project-file "gleam.toml"
+				    :compile "gleam build"
+				    :test "gleam test"
+				    :run "gleam run"
+                                    :src-dir "src/"
+                                    :test-dir "test/"
+				    :test-suffix "_test"))
+
+(after! eglot
+  (add-to-list 'eglot-server-programs
+               '((gleam-mode gleam-ts-mode) . ("gleam" "lsp"))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
