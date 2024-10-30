bd6aef9d84f6612f4cafddd50dd8caba8d5221ce
Lukas Czaplinski <lukasz@wooting.io>
2024-10-30 11:44:25 +0100

    Nice things for bat

--- modules/home.nix
+++ modules/home.nix
@@ -155,7 +155,25 @@
       };
     };
     dircolors = { enable = true; };
-    bat = { enable = true; };
+    bat = {
+      enable = true;
+      themes = {
+        catpuccin = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "d2bbee4f7e7d5bac63c054e4d8eca57954b31471";
+            sha256 = "sha256-x1yqPCWuoBSx/cI94eA+AWwhiSA42cLNUOFJl7qjhmw=";
+          };
+          file = "themes/Catppuccin Frappe.tmTheme";
+        };
+      };
+      config = {
+        paging = "never";
+        theme = "catppuccin";
+      };
+      extraPackages = with pkgs.bat-extras; [ batman ];
+    };
     btop = { enable = true; };
     atuin = {
       enable = true;
