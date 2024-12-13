1bc5a4783044fa60ec30fc6f80c25117a1f7c595
Lukas Czaplinski <lukasz@wooting.io>
2024-12-13 13:28:22 +0100

    fix: ncdu doesn't work on old Darwin

--- modules/cli.nix
+++ modules/cli.nix
@@ -2,19 +2,20 @@
 
 {
   # https://lobste.rs/s/xyvwux/what_are_your_favorite_non_standard_cli
-  home.packages = with pkgs; [
-    ncdu
-    tree
-    aerc
-    gron
-    nh
-    ugrep
-    svu
-    pv
-    doggo
-    fx
-    gitu
-    (pkgs.callPackage ../packages/cometary { })
-    (pkgs.callPackage ../packages/human { })
-  ];
+  home.packages = with pkgs;
+    [
+      tree
+      aerc
+      gron
+      nh
+      ugrep
+      svu
+      pv
+      doggo
+      fx
+      gitu
+      (pkgs.callPackage ../packages/cometary { })
+      (pkgs.callPackage ../packages/human { })
+    ] ++ (lib.lists.optional (with pkgs.stdenv.hostPlatform;
+      isDarwin -> lib.versionAtLeast darwinSdkVersion "11.0") ncdu);
 }
