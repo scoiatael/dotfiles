a2120a28736e15a586c79d6a5538a356e0ac63b0
Lukas Czaplinski <lukasz@wooting.io>
2024-12-27 18:07:31 +0100

    Back to rails

--- config/doom/config.el
+++ config/doom/config.el
@@ -35,6 +35,9 @@
 
   (cl-callf append elisp-flymake-byte-compile-load-path load-path))
 
+(add-hook! #'haml-mode
+  (setq-local tab-width 2))
+
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
 ;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
 ;;
