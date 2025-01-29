bbf21c1eb8f7f7d3586e79e50a98de730989ecb1
Lukasz Czaplinski <git@scoiatael.dev>
2025-01-29 20:47:42 +0100

    Fix login for anilist

--- modules/fastanime.nix
+++ modules/fastanime.nix
@@ -4,10 +4,14 @@
     pkgs.mpv
     # HACK: plyer required by upstream is broken on macOS
     # HACK: thefuzz required by upstream is broken on macOS
+    # HACK: login doesn't work on macOS
     (fastanime.packages."${pkgs.stdenv.hostPlatform.system}".default.overrideAttrs {
       preBuild = ''
         sed -i 's/rich>=13.9.2/rich>=13.8.1/' pyproject.toml
         sed -i 's/pycryptodome>=3.21.0/pycryptodome>=3.20.0/' pyproject.toml
+
+        sed -i 's/, wait=True/, wait=False/' fastanime/cli/commands/anilist/login.py
+        sed -i 's/Prompt.ask("Enter token")/open(Prompt.ask("Enter filename with token: "), "r").read().strip()/' fastanime/cli/commands/anilist/login.py
       '';
       propagatedBuildInputs = with pkgs.python312.pkgs; [
         click
