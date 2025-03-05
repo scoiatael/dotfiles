915bb60f69c7b9982b3d06f91bab1bca2ded46ce
Lukas Czaplinski <lukasz@wooting.io>
2025-03-05 17:48:22 +0100

    Add nix run alias

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -31,6 +31,7 @@
       n = "nix";
       ns = "nix-shell";
       nf = "nix flake";
+      nr = "nix run";
       nix-repl = "nix repl -f '<nixpkgs>'";
       zsh_stats = ''
         atuin history list --cmd-only | awk '{CMD[$1]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n10'';
