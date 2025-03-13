4452c83e30a1a7d51b02f7b8f8df83edb6d8e0ef
Lukas Czaplinski <lukasz@wooting.io>
2025-03-13 12:57:11 +0100

    configure process compose

--- modules/cli.nix
+++ modules/cli.nix
@@ -1,6 +1,11 @@
 { config, lib, pkgs, ... }:
 
-{
+let
+  processComposeConfigDir = if pkgs.stdenv.isDarwin then
+    "Library/Application Support"
+  else
+    config.xdg.configHome;
+in {
   # https://lobste.rs/s/xyvwux/what_are_your_favorite_non_standard_cli
   home.packages = with pkgs;
     [
@@ -20,4 +25,9 @@
       (pkgs.callPackage ../packages/human { })
     ] ++ (lib.lists.optional (with pkgs.stdenv.hostPlatform;
       isDarwin -> lib.versionAtLeast darwinSdkVersion "11.0") ncdu);
+
+  home.file."${processComposeConfigDir}/process-compose/theme.yaml".source =
+    ../config/process-compose/catppuccin-frappe.yaml;
+  home.file."${processComposeConfigDir}/process-compose/settings.yaml".source =
+    ../config/process-compose/settings.yaml;
 }
