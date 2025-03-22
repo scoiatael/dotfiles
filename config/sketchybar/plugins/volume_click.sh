75b68e1704ff5c76ec2359a29a306ec0795a0704
Lukasz Czaplinski <git@scoiatael.dev>
2025-03-22 18:01:59 +0100

    Fix volume click

--- config/sketchybar/plugins/volume_click.sh
+++ config/sketchybar/plugins/volume_click.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 NAME="${NAME:-volume}"
+SwitchAudioSource="/opt/homebrew/bin/SwitchAudioSource"
 toggle_devices() {
-  which SwitchAudioSource >/dev/null || exit 0
   source "$HOME/dotfiles/config/sketchybar/colors.sh"
 
   args=(--remove '/volume.device\.*/' --set "$NAME" popup.drawing=toggle)
   COUNTER=0
-  CURRENT="$(SwitchAudioSource -t output -c)"
+  CURRENT="$($SwitchAudioSource -t output -c)"
   while IFS= read -r device; do
     COLOR=$GREY
     if [ "${device}" = "$CURRENT" ]; then
@@ -16,9 +16,9 @@ toggle_devices() {
     args+=(--add item volume.device.$COUNTER popup."$NAME" \
            --set volume.device.$COUNTER label="${device}" \
                                         label.color="$COLOR" \
-                 click_script="SwitchAudioSource -s \"${device}\" && sketchybar --set /volume.device\.*/ label.color=$GREY --set \$NAME label.color=$WHITE --set $NAME popup.drawing=off")
+                 click_script="$SwitchAudioSource -s \"${device}\" && sketchybar --set /volume.device\.*/ label.color=$GREY --set \$NAME label.color=$WHITE --set $NAME popup.drawing=off")
     COUNTER=$((COUNTER+1))
-  done <<< "$(SwitchAudioSource -a -t output)"
+  done <<< "$($SwitchAudioSource -a -t output)"
 
   sketchybar -m "${args[@]}" > /dev/null
 }
