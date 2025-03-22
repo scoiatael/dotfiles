4c1d2a06707c7cafc92b871db9f212bb2df55e91
Lukasz Czaplinski <git@scoiatael.dev>
2025-03-22 23:29:57 +0100

    More emacs fun with Raycast

---
+++ config/raycast/emacs.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Emacs:
+# @raycast.mode inline
+#
+# Optional parameters:
+# @raycast.packageName Emacs
+# @raycast.icon images/emacs.png
+#
+# Conditional parameters:
+# @raycast.refreshTime 5m
+
+RED = 31
+GREEN = 32
+YELLOW = 33
+
+projects = `$HOME/.nix-profile/bin/emacsclient -e "(projectile-open-projects)"`[1..-1].split
+
+if projects.length == 0
+  message = "No open pull requests 🎉"
+  puts "\e[#{GREEN}m#{message}\e[0m"
+else
+  color = (projects.length <= 5) ? YELLOW : RED
+  message = "You have #{projects.length} open projects"
+  puts "\e[#{color}m#{message}\e[0m"
+end
