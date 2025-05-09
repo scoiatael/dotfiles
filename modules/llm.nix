a90693321c21bbdf525b5bb1ec03163d5017b48d
Lukasz Czaplinski <git@scoiatael.dev>
2025-05-09 16:08:50 +0200

    Make it work on Linux also

--- modules/llm.nix
+++ modules/llm.nix
@@ -1,6 +1,11 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-{
+let
+  configHome = if pkgs.stdenv.isDarwin then
+    "Library/Application Support"
+  else
+    config.xdg.configHome;
+in {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "claude-code" ];
   home.packages = with pkgs; [
@@ -9,6 +14,6 @@
     (pkgs.callPackage ../packages/llm-with-plugins { })
   ];
 
-  home.file."Library/Application Support/io.datasette.llm/default_model.txt".text =
+  home.file."${configHome}/io.datasette.llm/default_model.txt".text =
     "anthropic/claude-3-5-haiku-latest";
 }
