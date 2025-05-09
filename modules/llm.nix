4c8c4537a4fed828b714e59f3972e4251db7f111
Lukasz Czaplinski <git@scoiatael.dev>
2025-05-09 15:32:25 +0200

    Add llm package with anthropic as default

--- modules/llm.nix
+++ modules/llm.nix
@@ -3,5 +3,12 @@
 {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "claude-code" ];
-  home.packages = with pkgs; [ claude-code ];
+  home.packages = with pkgs; [
+    claude-code
+    # llm.withPlugins has been deprecated
+    (pkgs.callPackage ../packages/llm-with-plugins { })
+  ];
+
+  home.file."Library/Application Support/io.datasette.llm/default_model.txt".text =
+    "anthropic/claude-3-5-haiku-latest";
 }
---
+++ packages/llm-with-plugins/default.nix
@@ -0,0 +1,23 @@
+{ lib, pkgs, ... }:
+
+let
+  pythonEnv = pkgs.python3.withPackages (pp: with pp; [ llm llm-anthropic ]);
+  pass = lib.getExe pkgs.pass;
+in pkgs.stdenvNoCC.mkDerivation rec {
+  name = "llm";
+  dontBuild = true;
+
+  unpackPhase = "true";
+
+  bin = pkgs.writeShellScript "llm" ''
+    export ANTHROPIC_API_KEY="$(${pass} anthropic-com-api-token)"
+    exec ${pythonEnv}/bin/llm "''${@}"
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r ${bin} $out/bin/llm
+  '';
+
+  meta.mainProgram = "llm";
+}
