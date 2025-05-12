04342faf655d618aea453c7b073525cfa5c63634
Lukas Czaplinski <lukasz@wooting.io>
2025-05-12 16:46:50 +0200

    Use LLM to organise some work

--- modules/llm.nix
+++ modules/llm.nix
@@ -5,6 +5,9 @@ let
     "Library/Application Support"
   else
     config.xdg.configHome;
+  pdf-convert = pkgs.writeShellScriptBin "pdf-to-jpg" ''
+    convert -density 300 "$1" -quality 100 "''${1%.*}.jpg"
+  '';
 in {
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [ "claude-code" ];
@@ -12,8 +15,17 @@ in {
     claude-code
     # llm.withPlugins has been deprecated
     (pkgs.callPackage ../packages/llm-with-plugins { })
+
+    # for PDF conversion
+    ghostscript
+    pdf-convert
   ];
 
   home.file."${configHome}/io.datasette.llm/default_model.txt".text =
     "anthropic/claude-3-5-haiku-latest";
+
+  home.activation.linkLLMTemplates =
+    config.lib.dag.entryAfter [ "writeBoundary" ] ''
+      ln -sf  "${config.home.homeDirectory}/dotfiles/config/llm/templates" "${configHome}/io.datasette.llm/templates"
+    '';
 }
