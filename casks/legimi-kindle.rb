74727fb554dc535ecffc0df998f75e75f2a22986
Lukasz Czaplinski <git@scoiatael.dev>
2025-06-08 16:54:35 +0200

    Add checksum for legimi-kindle

--- casks/legimi-kindle.rb
+++ casks/legimi-kindle.rb
@@ -1,6 +1,6 @@
 cask "legimi-kindle" do
   version "0.1.0"
-  sha256 :no_check
+  sha256 "d27358d1114791de74a62c2b317ac793a7cfdc3fb8f49ea8dfaf2d94b377330d"
 
   url "https://files.legimi.com/static/installers/legimi-kindle.dmg"
   name "Legimi"
@@ -19,6 +19,6 @@ cask "legimi-kindle" do
     "~/Library/Preferences/com.legimi.kindle.plist",
     "~/Library/HTTPStorages/com.legimi.kindle/*",
     "~/Library/Application Support/com.legimi.kindle/*",
-    "~/Library/Caches/com.legimi.kindle/*"
+    "~/Library/Caches/com.legimi.kindle/*",
   ]
 end
