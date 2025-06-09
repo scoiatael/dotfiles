1541297def277c34b9c54664bae958cdf31e2e35
Lukas Czaplinski <lukasz@wooting.io>
2025-06-09 12:16:53 +0200

    Simplify llm derivation

--- packages/llm-with-plugins/default.nix
+++ packages/llm-with-plugins/default.nix
@@ -3,22 +3,8 @@
 let
   pythonEnv = pkgs.python3.withPackages (pp: with pp; [ llm llm-anthropic ]);
   pass = lib.getExe pkgs.pass;
-in pkgs.stdenvNoCC.mkDerivation rec {
-  name = "llm";
-  dontBuild = true;
-
-  unpackPhase = "true";
-
-  bin = pkgs.writeShellScript "llm" ''
-    unset PYTHONPATH # Otherwise it breaks in Python devenvs
-    export ANTHROPIC_API_KEY="$(${pass} anthropic-com-api-token)"
-    exec ${pythonEnv}/bin/llm "''${@}"
-  '';
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp -r ${bin} $out/bin/llm
-  '';
-
-  meta.mainProgram = "llm";
-}
+in pkgs.writeShellScriptBin "llm" ''
+  unset PYTHONPATH # Otherwise it breaks in Python devenvs
+  export ANTHROPIC_API_KEY="$(${pass} anthropic-com-api-token)"
+  exec ${pythonEnv}/bin/llm "''${@}"
+''
