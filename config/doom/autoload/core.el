4be8bac3dd44b120bd89742134b5cd8463d15c65
Lukas Czaplinski <lukasz@wooting.io>
2025-07-17 13:50:17 +0200

    fix agenda switch

--- config/doom/autoload/core.el
+++ config/doom/autoload/core.el
@@ -101,7 +101,7 @@ current window."
   "Switch to workspace named AGENDA or create it if it doesn't exist."
   (interactive)
   (let ((workspace-name "AGENDA"))
-    (unless (= (+workspace-current) +workspaces-main)
+    (unless (eq (+workspace-current-name) +workspaces-main)
       (unless (+workspace-exists-p workspace-name)
         (+workspace-new workspace-name))
       (+workspace-switch workspace-name))
