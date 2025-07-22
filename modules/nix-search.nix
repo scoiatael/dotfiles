eaaf8a00625e6ea51f7dde5e9519ca8297eb5ce0
Lukas Czaplinski <lukasz@wooting.io>
2025-07-22 10:45:27 +0200

    add manix

--- modules/nix-search.nix
+++ modules/nix-search.nix
@@ -3,7 +3,10 @@
 
 let nix-search = nixSearch.packages.${pkgs.system}.default;
 in {
-  home.packages = [ nix-search ];
+  home.packages = [
+    nix-search
+    pkgs.manix # CLI docs for nix
+  ];
 
   home.activation.indexNixSearch =
     config.lib.dag.entryAfter [ "writeBoundary" ] ''
