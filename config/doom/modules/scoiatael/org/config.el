e2809cdeac8b75c362cfa7dab807c87b23d31366
Lukas Czaplinski <lukasz@wooting.io>
2025-09-01 15:35:30 +0200

    fix emacs org clash

--- config/doom/modules/scoiatael/org/config.el
+++ config/doom/modules/scoiatael/org/config.el
@@ -5,7 +5,7 @@
 (use-package! org-reverse-datetree
   :config
   (map! :map org-mode-map :localleader
-        "d" #'org-reverse-datetree-goto-date-in-file
+        "D" #'org-reverse-datetree-goto-date-in-file
         "a" #'org-reverse-datetree-archive-subtree))
 
 (after! org
