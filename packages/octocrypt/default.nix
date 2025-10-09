dcc7f9a1b629cca42d7b7feba2127c6582740428
Lukasz Czaplinski <git@scoiatael.dev>
2025-10-09 22:16:42 +0200

    octocrypt: fix dependencies

--- packages/octocrypt/default.nix
+++ packages/octocrypt/default.nix
@@ -1,23 +1,25 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 
 let
-  runFile = pkgs.writeShellScript "octocrypt" ''
-    cd $(dirname "$0")/../ || exit 1
-    ${pkgs.clojure}/bin/clj -Srepro -J-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -X octocrypt.server/main
-  '';
+  runFile = pkgs.writeShellApplication {
+    name = "octocrypt";
+    runtimeInputs = with pkgs; [ age gnupg clojure ];
+    text = ''
+      cd "$(dirname "$0")/../" || exit 1
+      clj -Srepro -J-Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager -X octocrypt.server/main
+    '';
+  };
 in pkgs.stdenv.mkDerivation {
   name = "octocrypt";
   src = ./.;
-  text = "";
   phases = [ "unpackPhase" "buildPhase" "installPhase" ];
-  buildInputs = [ pkgs.age pkgs.gnupg ];
 
   buildPhase = ''
     mkdir -p $out/bin
     cp -r $src/src $out/src
     cp -r $src/public $out/public
     cp -r $src/deps.edn $out/deps.edn
-    cp ${runFile} $out/bin/octocrypt
+    cp ${pkgs.lib.getExe runFile} $out/bin/octocrypt
   '';
 
 }
