5fc39335bd997fce56b7ed55190501e235bc7f94
Lukasz Czaplinski <git@scoiatael.dev>
2026-01-10 20:36:59 +0100

    framework: declarative deluge config

--- nixos/modules/deluge.nix
+++ nixos/modules/deluge.nix
@@ -4,5 +4,8 @@
   services.deluge = {
     enable = true;
     web.enable = true;
+    declarative = true;
+    authFile = "/etc/nixos/secrets/deluge-auth";
+    config = { download_location = "/srv/nfs/quarantine/"; };
   };
 }
