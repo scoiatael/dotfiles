4f190ae5354bfabfbec3b55ab0d694c25a5fe273
Lukasz Czaplinski <git@scoiatael.dev>
2026-01-11 15:26:24 +0100

    framework: set deluge options

--- nixos/modules/deluge.nix
+++ nixos/modules/deluge.nix
@@ -6,6 +6,10 @@
     web.enable = true;
     declarative = true;
     authFile = "/etc/nixos/secrets/deluge-auth";
-    config = { download_location = "/srv/nfs/quarantine/"; };
+    config = {
+      download_location = "/srv/nfs/quarantine/";
+      max_upload_speed = "1000.0";
+      share_ratio_limit = "0.5";
+    };
   };
 }
