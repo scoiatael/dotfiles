48e173b456ac38757bf001ebe39ba0848915a721
Lukasz Czaplinski <git@scoiatael.dev>
2026-02-24 22:58:18 +0100

    darwin: clean up apps

--- modules/darwin/air.nix
+++ modules/darwin/air.nix
@@ -20,18 +20,15 @@
       "bettertouchtool"
       "karabiner-elements"
       "balenaetcher"
-      "keymapp"
-      "proton-mail"
       "proton-pass"
       "arc"
       "secretive"
       "scoiatael/dotfiles/legimi-kindle"
       "astropad-studio"
+      "tailscale-app"
     ];
     masApps = {
       "bitwarden" = 1352778147;
-      "kagi" = 1622835804;
-      "hush" = 1544743900;
       "Affinity Photo 2" = 1616822987;
       "DaisyDisk" = 411643860;
       "Proton Pass for Safari" = 6502835663;
