04f5190fa92b87c9e50bc7de14dfdb2dd2c35c3d
Lukasz Czaplinski <git@scoiatael.dev>
2026-03-06 19:28:03 +0100

    framework: prevent clamd from waking me up

--- nixos/modules/clamav.nix
+++ nixos/modules/clamav.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   services.clamav = {
@@ -6,8 +11,20 @@
     daemon.enable = true;
     scanner = {
       enable = true;
-      scanDirectories =
-        [ "/home" "/var/lib" "/tmp" "/etc" "/var/tmp" "/srv/nfs/quarantine" ];
+      scanDirectories = [
+        "/home"
+        "/var/lib"
+        "/tmp"
+        "/etc"
+        "/var/tmp"
+        "/srv/nfs/quarantine"
+      ];
     };
+  };
+
+  # Allow 2 CPUs [[id:c4c4a950-8dbc-4b35-9358-c79c59cb317a][systemd-resource-control]]
+  # Otherwise it uses all of them (not very nice!)
+  systemd.services.clamav-daemon.serviceConfig = {
+    CPUQuota = "200%";
   };
 }
