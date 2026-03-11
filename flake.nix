73330f7d703fc16a32dbd7daaf13ae6cc5751542
Lukas Czaplinski <lukasz@wooting.io>
2026-03-11 17:52:48 +0100

    nix: switch to nixos-25.11 instead of unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs.nix-index-database.url = "github:nix-community/nix-index-database";
   inputs.nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   inputs.home-manager = {
     url = "github:nix-community/home-manager";
     inputs.nixpkgs.follows = "nixpkgs";
