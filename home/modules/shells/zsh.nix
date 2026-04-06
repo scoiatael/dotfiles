6251212fe229b3a42a48ba3ba779dd93363b368e
Lukasz Czaplinski <git@scoiatael.dev>
2026-04-06 15:38:47 +0200

    zsh: rework compinit again

--- home/modules/shells/zsh.nix
+++ home/modules/shells/zsh.nix
@@ -43,9 +43,11 @@ in
         '';
         poi = "gh poi --dry-run; read -q 'ok? [Y/y]' && gh poi || echo 'aborted by prompt' ";
         hmr = "${nh} home switch 'path:${config.home.homeDirectory}/dotfiles' -b bp.$(${date} --iso-8601)";
-        os-switch = if pkgs.hostPlatform.isDarwin then "dnr" else "nor";
-        nor = "doas ${nh} os switch -R 'path:${config.home.homeDirectory}/dotfiles'";
-        dnr = "${nh} darwin switch 'path:${config.home.homeDirectory}/dotfiles'";
+        os-switch =
+          if pkgs.stdenv.hostPlatform.isDarwin then
+            "${nh} darwin switch 'path:${config.home.homeDirectory}/dotfiles'"
+          else
+            "doas ${nh} os switch -R 'path:${config.home.homeDirectory}/dotfiles'";
         nix-test = "nix-build --keep-failed --expr 'with import <nixpkgs> {}; callPackage ./default.nix {}'";
         nix-test-python = "nix-build --keep-failed --expr 'let pkgs = import <nixpkgs> {}; in pkgs.python3Packages.callPackage ./default.nix {}'";
         nix-tree-devshell = "nix-tree --derivation '.#devShells.${pkgs.stdenv.hostPlatform.system}.default'";
@@ -74,7 +76,9 @@ in
         gpg-quick-expire-extend = ''gpg --quick-set-expire "$(gpg-fpr)" 3m && gpg --quick-set-expire "$(gpg-fpr)" 3m $(gpg-subkeys)'';
         random = ''ruby -r securerandom -e "puts SecureRandom.hex(ARGV.first&.to_i || 32)" "''${@}"'';
         notmuch-ui = "emacs -nw -f notmuch";
-        "mkinvoice" = ''mkdir "Invoices:$(date "+%y%m")"'';
+        # https://gist.github.com/elalemanyo/cb3395af64ac23df2e0c3ded8bd63b2f
+        zshtime = "time ZSH_DEBUGRC=1 zsh -i -c exit";
+        zshcompinit = "time ZSH_COMPINIT=1 zsh -i -c exit";
       }
       // (lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
         # NOTE: Available since macOS 15
@@ -109,12 +113,33 @@ in
         export ZSH_AUTOSUGGEST_MANUAL_REBIND=false
         export NIXD_FLAGS="--inlay-hints=false"
         export FZF_CTRL_T_COMMAND="fd --type f --hidden --follow --exclude .git --exclude .devenv --exclude .direnv"
+        if [ -n "''${ZSH_DEBUGRC+1}" ]; then
+            zmodload zsh/zprof
+        fi
+      '')
+      # https://github.com/theunraveler/zsh-fancy_ctrl_z/blob/master/fancy_ctrl_z.plugin.zsh
+      (lib.mkOrder 550 ''
+        fancy-ctrl-z () {
+          if [[ $#BUFFER -eq 0 ]]; then
+            BUFFER="fg"
+            zle accept-line
+          else
+            zle push-input
+            zle clear-screen
+          fi
+        }
+        zle -N fancy-ctrl-z
+        bindkey '^Z' fancy-ctrl-z
+      '')
+      (lib.mkOrder 570 ''
+        if  [[ ! -f ~/.zcompdump || -n "''${ZSH_COMPINIT+1}" ]]; then
+            autoload -Uz compinit && compinit
+        else
+            autoload -Uz compinit && compinit -C # assume zcompdump is fresh
+        fi
       '')
       (lib.mkAfter ''
         source ~/dotfiles/config/qq.zsh
-        autoload -U compinit
-        compinit -C # assume zcompdump is fresh
-
         bindkey "" history-beginning-search-forward
         bindkey "" history-beginning-search-backward
         bindkey '^W' backward-delete-word
@@ -128,32 +153,11 @@ in
 
         # TODO: fix on non-Darwin
         path+=("/opt/homebrew/bin/" "$HOME/dotfiles/bin" "$HOME/.emacs.doom/bin")
+        if [ -n "''${ZSH_DEBUGRC+1}" ]; then
+            zprof
+        fi
       '')
     ];
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "tmux"
-        "gpg-agent"
-        "fancy-ctrl-z"
-        "dircycle"
-        "gitfast"
-      ];
-      extraConfig = ''
-        ### Fix slowness of pastes with zsh-syntax-highlighting.zsh
-        pasteinit() {
-          OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
-          zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
-        }
-
-        pastefinish() {
-          zle -N self-insert $OLD_SELF_INSERT
-        }
-        zstyle :bracketed-paste-magic paste-init pasteinit
-        zstyle :bracketed-paste-magic paste-finish pastefinish
-        ### Fix slowness of pastes
-      '';
-    };
     plugins = with pkgs; [
       {
         name = "forgit";
@@ -200,25 +204,11 @@ in
   };
   programs.starship.enableZshIntegration = true;
 
-  home.activation.updateDotzcompdump =
+  home.activation.updateZcompdump =
     let
-      script = pkgs.writeTextFile {
-        name = "update.zsh";
-        executable = true;
-        text = ''
-          #! ${pkgs.zsh}/bin/zsh -i
-          rm -rf "$HOME"/.zcompdump*
-          autoload -U compinit
-        ''
-        + (lib.strings.optionalString config.programs.jujutsu.enable ''
-          source <(${pkgs.jujutsu}/bin/jj util completion zsh)
-          compdef _jj ${pkgs.jujutsu}/bin/jj
-        '')
-        + ''
-          source <(${pkgs.just}/bin/just --completions zsh)
-          compinit
-        '';
-      };
+      script = pkgs.writeShellScript "update-zcompdump" ''
+        rm -rf ~/.zcompdump*
+      '';
     in
     config.lib.dag.entryAfter [ "writeBoundary" ] "${script}";
 }
