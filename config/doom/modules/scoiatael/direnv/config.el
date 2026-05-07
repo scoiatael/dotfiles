e821bb9fb7c8e369e2be569ed4a1efc57503e52e
Lukas Czaplinski <lukasz@wooting.io>
2026-05-07 14:09:24 +0200

    Revert "emacs: migrate envrc->ben"
    This reverts commit fb6d74c054b62e97c0fb927a1795d35e57f59616.
    Suspected of breaking eglot

--- config/doom/modules/scoiatael/direnv/config.el
+++
@@ -1,6 +0,0 @@
-;;; scoiatael/direnv/config.el -*- lexical-binding: t; -*-
-
-(use-package! ben
-  :config
-  (add-hook 'doom-first-file-hook #'ben-global-mode))
-
--- config/doom/modules/scoiatael/direnv/packages.el
+++
@@ -1,7 +0,0 @@
-;;; scoiatael/direnv/packages.el -*- lexical-binding: t; -*-
-
-(package! ben :recipe (:type git
-                       :host codeberg
-                       :repo "pastor/ben.el"))
-
-
