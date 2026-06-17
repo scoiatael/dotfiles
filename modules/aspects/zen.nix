28e3c9e76b535b0fded379fd97d329c7e9b30739
Lukas Czaplinski <lukasz@wooting.io>
2026-06-17 11:38:57 +0200

    zen: tweaks

--- modules/aspects/zen.nix
+++ modules/aspects/zen.nix
@@ -3,7 +3,7 @@
 {
   flake-file.inputs.zen-browser.url = "github:0xc000022070/zen-browser-flake";
   den.aspects.zen = {
-    includes = [den.aspects.stylix];
+    includes = [ den.aspects.stylix ];
     homeManager =
       {
         config,
@@ -100,31 +100,21 @@
             {
               id = "zen-compact-mode-toggle";
               key = "s";
-              modifiers.control = true;
-              modifiers.alt = true;
+              modifiers.accel = true;
             }
             {
               id = "zen-compact-mode-show-sidebar"; # toggle floating sideboar
               key = "f";
-              modifiers.control = true;
-              modifiers.alt = true;
-            }
-            {
-              id = "key_savePage";
-              key = "s";
-              modifiers.control = true;
-            }
-            {
-              id = "key_quitApplication";
-              disabled = true;
+              modifiers.shift = true;
+              modifiers.accel = true;
             }
           ];
 
           spacesForce = true;
           spaces = {
-            "Personal" = {
+            "Music" = {
               id = "4d929899-3c7c-44e3-be00-e1e850836b6f";
-              icon = "🏡";
+              icon = "🎸";
               position = 1000;
               theme = {
                 type = "gradient";
@@ -147,8 +137,8 @@
             };
             "Work" = {
               id = "1aa8cdd7-cf7b-4523-a2aa-20d3f085dfd3";
-              icon = "🧑‍💻";
-              position = 2000;
+              icon = "⌘";
+              position = 500;
               theme = {
                 type = "gradient";
                 colors = [
@@ -175,87 +165,81 @@
           pins = {
             "GCal" = {
               id = "336445e0-1f54-45cb-a83a-83d0d59f4d74";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://calendar.google.com";
               isEssential = true;
-              position = 0;
             };
             "Gather v1" = {
               id = "097764b9-eae6-4961-8a43-005616d80de4";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://app.gather.town/app/oiZp6OnQZKD7RX51/Wooting%20World";
               isEssential = true;
-              position = 1;
             };
             "Gather v2" = {
               id = "31d894db-1f22-4007-9d0d-b3fa874dd69b";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://app.v2.gather.town/app/wooting-officehev2-bfb336c4-e33b-4fc5-aeeb-f5bbc2e4f910";
               isEssential = true;
-              position = 2;
             };
             "Notion" = {
               id = "40c7b93b-fb81-40ba-b9ce-0241f444cb07";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://www.notion.so/60eed1146a9c4240a7b0bd25d68fc240";
-              isEssential = true;
-              position = 3;
+              isEssential = false;
             };
             "Uptime Robot" = {
               id = "d8d610e5-dc09-4c52-b6e2-d663635069ec";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://dashboard.uptimerobot.com/login?rt=false";
-              isEssential = true;
-              position = 4;
+              isEssential = false;
             };
             "Digitalocean" = {
               id = "134b8cbd-f9b0-4b9e-9c5a-a0e5feaade56";
-              workspace = spaces."Work".id;
-              url = "https://cloud.digitalocean.com/apps?i=c36fc7";
+              workspace = spaces.Work.id;
+              url = "https://cloud.digitalocean.com/apps";
               isEssential = true;
-              position = 5;
             };
             "Honeycomb" = {
               id = "379437a9-0674-4b0e-8b1a-064c5ff91e2d";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://ui.honeycomb.io/wooting/environments/production/datasets/wooting-server/home?tab=traces";
               isEssential = true;
-              position = 6;
             };
             "Temporal" = {
               id = "7174abc7-94fa-41b7-8694-aed50668d5dd";
-              workspace = spaces."Work".id;
-              url = "https://temporal-ui.tail4b94e5.ts.net/namespaces/default/workflows?query=%60WorkflowId%60+STARTS_WITH+%22order-cancel-reassign-item%22";
-              isEssential = true;
-              position = 7;
+              workspace = spaces.Work.id;
+              url = "https://temporal-ui.tail4b94e5.ts.net/";
+              isEssential = false;
             };
             "Graphite" = {
               id = "9418efa6-8723-42e8-b745-e7401eae14a2";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://app.graphite.com/#needs-your-review";
               isEssential = true;
-              position = 8;
             };
             "JDSLabs" = {
               id = "747ea73e-8323-452d-baef-55b4a8624fd6";
-              workspace = spaces."Work".id;
+              workspace = spaces.Music.id;
               url = "https://core.jdslabs.com/";
-              isEssential = true;
-              position = 9;
+              isEssential = false;
             };
             "k3s" = {
               id = "0a1665e6-4d85-4c96-81f2-2b77c8d73e56";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://wt-k3s.tail4b94e5.ts.net/favorites";
-              isEssential = true;
-              position = 10;
+              isEssential = false;
             };
             "Tailscale" = {
               id = "6eb662ba-0a57-4f41-9cb1-f9381c464ec9";
-              workspace = spaces."Work".id;
+              workspace = spaces.Work.id;
               url = "https://login.tailscale.com/admin/machines";
-              isEssential = true;
-              position = 11;
+              isEssential = false;
+            };
+            "Linear" = {
+              id = "58a13bf2-34bf-4631-a3ee-45e6a2ddeecb";
+              workspace = spaces.Work.id;
+              url = "https://linear.app";
+              isEssential = false;
             };
           };
 
@@ -331,24 +315,6 @@
                   definedAliases = [ "hm" ];
                 };
 
-                "Google Maps" = {
-                  urls = [
-                    {
-                      template = "http://maps.google.com";
-                      params = [
-                        {
-                          name = "q";
-                          value = "{searchTerms}";
-                        }
-                      ];
-                    }
-                  ];
-                  definedAliases = [
-                    "maps"
-                    "gmaps"
-                  ];
-                };
-
                 "Kagi" = {
                   urls = [
                     {
@@ -378,6 +344,21 @@
                     }
                   ];
                   definedAliases = [ "h" ];
+                };
+
+                "Noogle" = {
+                  urls = [
+                    {
+                      template = "https://noogle.dev/q/";
+                      params = [
+                        {
+                          name = "term";
+                          value = "{searchTerms}";
+                        }
+                      ];
+                    }
+                  ];
+                  definedAliases = [ "n" ];
                 };
               };
           };
