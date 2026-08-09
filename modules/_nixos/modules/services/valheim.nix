fc11ef4b4e3bfaea1c5bde6ed59d6e23226eae28
Lukasz Czaplinski <git@scoiatael.dev>
2026-08-09 19:51:41 +0200

    fixup! sd-161581: deploy valheim server

--- modules/_nixos/modules/services/valheim.nix
+++ modules/_nixos/modules/services/valheim.nix
@@ -5,6 +5,34 @@
   ...
 }:
 
+let
+  # Combat: veryeasy, easy, hard, veryhard
+  # DeathPenalty: casual, veryeasy, easy, hard, hardcore
+  # Resources: muchless, less, more, muchmore, most
+  # Raids: none, muchless, less, more, muchmore
+  # Portals: casual, hard, veryhard
+  modifiers = {
+    Combat = "hard";
+    DeathPenalty = "casual";
+    Resources = "most";
+    Raids = "muchless";
+    Portals = "casual";
+  };
+  serverArgs = [
+    "-preset"
+    "normal"
+  ]
+  ++ (builtins.concatLists (
+    lib.mapAttrsToList (mod: val: [
+      "-modifier"
+      mod
+      val
+    ]) modifiers
+  ));
+  serverEnv = pkgs.writers.writeText "valheim-server-args" ''
+    SERVER_ARGS=${lib.escapeShellArgs serverArgs}
+  '';
+in
 {
   users.users.valheim = {
     isSystemUser = true;
@@ -12,8 +40,8 @@
     createHome = true;
     home = "/var/lib/valheim";
   };
-  users.users.restic.group = "restic";
-  users.groups.restic = { };
+  users.users.valheim.group = config.users.groups.valheim.name;
+  users.groups.valheim = { };
   # docker run -d \
   #     --name valheim-server \
   #     --cap-add=sys_nice \
@@ -25,21 +53,28 @@
   #     -e WORLD_NAME="Neotopia" \
   #     -e SERVER_PASS="secret" \
   #     ghcr.io/community-valheim-tools/valheim-server
+  systemd.tmpfiles.rules = [
+    "d ${config.users.users.valheim.home}/config 0700 ${config.users.users.valheim.name} ${config.users.groups.valheim.name} -"
+    "d ${config.users.users.valheim.home}/data 0700 ${config.users.users.valheim.name} ${config.users.groups.valheim.name} -"
+  ];
   virtualisation.oci-containers.containers.valheim-server = {
     image = "ghcr.io/community-valheim-tools/valheim-server";
-    environmentFiles = [ config.sops.secrets.valheim-server-env.path ];
+    environmentFiles = [
+      config.sops.secrets.valheim-server-env.path
+      serverEnv
+    ];
     volumes = [
       "${config.users.users.valheim.home}/config:/config"
       "${config.users.users.valheim.home}/data:/opt/valheim"
     ];
     ports = [
-      "127.0.0.1:2456-2457:2456-2457/udp"
+      "2456-2457:2456-2457/udp"
     ];
     capabilities = {
       SYS_NICE = true;
     };
     extraOptions = [
-      "--stop-timeout 120"
+      "--stop-timeout=120"
     ];
   };
 }
