9f37a08ad62e6c6ff6634d1ff46d61e97bec55c8
Lukasz Czaplinski <git@scoiatael.dev>
2026-08-25 21:53:48 +0200

    emacs: fix combobulate dependency chain: transient->llama

--- config/doom/modules/scoiatael/web/packages.el
+++ config/doom/modules/scoiatael/web/packages.el
@@ -2,4 +2,6 @@
 
 (package! reason-mode :recipe (:host github :repo "reasonml-editor/reason-mode") :pin "d657ff75572a8ea7eda6fe22ada3a2ebf5bc6119")
 (package! rescript-mode :recipe (:host github :repo "jjlee/rescript-mode") :pin "b9dda43cc52cd4a9b384c59fb0cc7a11f0bad230")
+(package! transient :recipe (:host github :repo "magit/transient"))
+(package! llama :recipe (:host github :repo "tarsius/llama"))
 (package! combobulate :recipe (:repo "mickeynp/combobulate" :host github) :pin "713bf3081f2d80cbd13ed175a808b242d9cc652d")
