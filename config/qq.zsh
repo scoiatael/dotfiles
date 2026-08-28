66adcfe44e8138be5096f7cd2cc9739f3244a9e2
Lukas Czaplinski <lukasz@wooting.io>
2026-08-28 10:22:02 +0200

    qq: fix problem with new llm

--- config/qq.zsh
+++ config/qq.zsh
@@ -1,11 +1,16 @@
 # Based on https://github.com/dzervas/dotfiles/blob/0e52489/home/fish-functions/qq.fish
 function qq() {
+    local ANSWER cmd
     # { "commandline": "ss -tuln", "comments": "alternatively 'netstat -tuln" }
     ANSWER="$(echo "${@}" | llm -t qq -p system "$(uname -a)")"
-    if [[ "$ANSWER" == *comments* ]];then
+    if ! echo "$ANSWER" | jq -e . &> /dev/null; then
+        print -P "%F{red}qq error:%f $ANSWER" >&2
+        return 1
+    fi
+    if echo "$ANSWER" | jq -e 'has("comments")' &>/dev/null; then
         print -P "$(echo "$ANSWER" | jq -r '.comments')"
     fi
-    if [[ "$ANSWER" == *commandline* ]];then
+    if echo "$ANSWER" | jq -e 'has("commandline")' &>/dev/null; then
         CMD="$(echo "$ANSWER" | jq -r '.commandline')"
         print -z "$CMD"
     fi
